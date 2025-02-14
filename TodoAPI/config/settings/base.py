ac331c5dfcee55d5b31b628ef4ed2f5608165cb6
nghiahm <nghiahm@fez-inc.jp>
2025-02-14 08:15:02 +0000

    feat: add todos app

--- TodoAPI/config/settings/base.py
+++ TodoAPI/config/settings/base.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "rest_framework.authtoken",
     # Local apps
     "users",
+    "todos",
 ]
 
 MIDDLEWARE = [
---
+++ TodoAPI/todos/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Todo
+
+
+admin.site.register(Todo)
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "completed", "created_at")
