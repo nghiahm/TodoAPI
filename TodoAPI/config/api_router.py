eb8d793b4330891b8f282db05e95d59bd05ce25b
nghiahm <nghiahm@fez-inc.jp>
2025-02-14 03:10:38 +0000

    feat: add obtain auth token

--- TodoAPI/config/api_router.py
+++ TodoAPI/config/api_router.py
@@ -1,7 +1,13 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from users.api.views import UserViewSet
 
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="user")
 urlpatterns = router.urls
+
+urlpatterns += [
+    path("auth-token/", obtain_auth_token),
+]
